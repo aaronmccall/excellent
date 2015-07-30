3871964126e606e4ccc170a6f0602dfa8c287c90
Aaron McCall <aaron@andyet.net>
2015-07-30 17:16:03 -0600

    remove old examples

--- examples/itineraryExport/styles/compileStyles.js
+++
@@ -1,7 +0,0 @@
-// TODO: move his logic to an async method in styles.js
-var jade = require('jade');
-var styleData = require('./styles');
-
-var template = jade.compileFile(__dirname + '/styles.jade', {doctype: 'xml'});
-
-console.log(template(styleData));
