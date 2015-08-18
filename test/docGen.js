6e056b53b0c2c44395e00ede01fbff8275a378e0
Aaron McCall <aaron@andyet.net>
2015-08-18 17:58:25 -0600

    smoke test

---
+++ test/docGen.js
@@ -0,0 +1,32 @@
+var Document = require('../lib/document');
+var fs = require('fs');
+var dkGreyBorder = {style: 'thin', color: 'Charcoal Gray'};
+var doc = new Document({
+    sheets: {
+        'Summary': [{
+            cells: [
+                'foo',
+                {value: 'bar', style: 'bold'},
+                {value: 'foo', style: 'lemonBg'},
+                'baz',
+                {value: 'quux', style: 'lemonBgBold'}
+            ]
+        }, {
+            cells: ['', {value: 'WAT?!', style: 'brick'}, {value: 'dotty', style: 'dotty'}]
+        }]
+    },
+    styles: {
+        borders: [{label: 'dkGrey', left: dkGreyBorder, right: dkGreyBorder, top: dkGreyBorder, bottom: dkGreyBorder}],
+        fonts: [{label: 'bold', bold: true}, {label: 'brick', color: 'Brick Red'}],
+        fills: [{label: 'lemon', type: 'pattern', color: 'Lemon Glacier'}],
+        cellStyles: [
+            {label: 'bold', font: 'bold'},
+            {label: 'brick', font: 'brick', border: 'dkGrey'},
+            {label: 'lemonBg', fill: 'lemon'},
+            {label: 'lemonBgBold', font: 'bold', fill: 'lemon'},
+            {label: 'dotty', fill: 1}
+        ]
+    }
+});
+
+fs.writeFileSync(__dirname + '/test.xlsx', doc.toXLSX());
