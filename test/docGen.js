074a6b657edffe6c4db85552b82b0dacd1ffb18c
Aaron McCall <aaron@andyet.net>
2015-08-19 16:22:15 -0600

    add image embedding to smoke test

--- test/docGen.js
+++ test/docGen.js
@@ -3,17 +3,20 @@ var fs = require('fs');
 var dkGreyBorder = {style: 'thin', color: 'Charcoal Gray'};
 var doc = new Document({
     sheets: {
-        'Summary': [{
-            cells: [
-                'foo',
-                {value: 'bar', style: 'bold'},
-                {value: 'foo', style: 'lemonBg'},
-                'baz',
-                {value: 'quux', style: 'lemonBgBold'}
-            ]
-        }, {
-            cells: ['', {value: 'WAT?!', style: 'brick'}, {value: 'dotty', style: 'dotty'}]
-        }]
+        'Summary': {
+            image: {image: fs.readFileSync(__dirname + '/assets/dog.png'), filename: 'dog.png'},
+            rows: [{
+                cells: [
+                    'foo',
+                    {value: 'bar', style: 'bold'},
+                    {value: 'foo', style: 'lemonBg'},
+                    'baz',
+                    {value: 'quux', style: 'lemonBgBold'}
+                ]
+            }, {
+                cells: ['', {value: 'WAT?!', style: 'brick'}, {value: 'dotty', style: 'dotty'}]
+            }]
+        }
     },
     styles: {
         borders: [{label: 'dkGrey', left: dkGreyBorder, right: dkGreyBorder, top: dkGreyBorder, bottom: dkGreyBorder}],
